65071065cf0ae4d61f522ca32f942e8b7c517c0f
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-27 17:46:08 -0300

    feat(Req12)
    implementado a query do requisito12

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+  { nome: { $nin: ["McChicken"] } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+  } },
+);
+db.produtos.find({}, { nome: true, ingredientes: true, _id: false });
