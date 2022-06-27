c4074fd313b7ad30d83ce7a14db46ba2d8ac3923
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-27 19:32:10 -0300

    feat(Req22)
    implementado a query do requisito22

---
+++ challenges/desafio22.js
@@ -0,0 +1,22 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+  } },
+);
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $inc: {
+      "vendasPorDia.3": +60,
+  } },
+);
+db.produtos.updateMany(
+  { tags: "bovino" },
+  {
+    $inc: {
+      "vendasPorDia.6": +120,
+  } },
+);
+db.produtos.find({}, { nome: true, vendasPorDia: true, _id: false });
